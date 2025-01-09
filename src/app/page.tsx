90ef1bbf14d5ca02a5f24410a15275b3e3528961
kyrylolvov <kyrylo@arccompute.io>
2025-01-09 13:06:31 -0500

    CHANGED: - Fixed footer note and links
    Signed-off-by: kyrylolvov <kyrylo@arccompute.io>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <main className="mx-auto flex h-full w-[700px] flex-col gap-4">
       <InvoiceForm />
-      <h1 className="text-center text-secondary-foreground/50">
+      <h1 className="text-center text-sm text-secondary-foreground/50">
         Open-Source Invoice Builder. Done by{" "}
-        <Link target="_blank" href="https://x.com/kyrylolvov">
+        <Link target="_blank" href="https://x.com/kyrylolvov" rel="noreferrer nofollow">
           @kyrylolvov
         </Link>{" "}
         and{" "}
-        <Link target="_blank" href="https://x.com/vladstohnii">
+        <Link target="_blank" href="https://x.com/vladstohnii" rel="noreferrer nofollow">
           @vladstohnii
         </Link>
       </h1>
